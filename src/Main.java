00da268db059fbf4667f33c3c368671e40e07ca9
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2020-10-15 12:09:37 +0200

    1 Grundgerüst

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public static void main(String[] args) {
+        System.out.print("Moin Leute");
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
